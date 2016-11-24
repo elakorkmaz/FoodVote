b1eafedb71d6e0e99fe5c6722290d0d34f672882
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-11-24 15:35:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+      displayRoutes = require('express-routemap'),
+      morgan = require('morgan'),
+      pug = require('pug'),
+      Sequelize = require('sequelize');
+
+var app = express(),
+    sequelize = new Sequelize('foodvote', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, { dialect: 'postgres' });
+
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+sequelize.sync().then(() => {
+  console.log('connected to database');
+  app.listen(3000, () => {
+    console.log('server is now running on port 3000');
+    displayRoutes(app);
+  });
+});
