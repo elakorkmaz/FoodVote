8df34a022950062bdc4ce53cd719c3f18b79b6a5
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-11-29 11:32:19 +0100

    some small changes

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require('express'),
       pug = require('pug'),
       Sequelize = require('sequelize'),
       fs = require('fs'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override');
+
+var db = require('./models');
 
 var app = express(),
     sequelize = new Sequelize('foodvote', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, { dialect: 'postgres' });
@@ -19,12 +22,26 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }})
+);
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/show', (req, res) => {
-  res.render('show');
+app.get('/:slug', (req, res) => {
+  db.Meal.findOne({
+    where: {
+      slug: req.params.slug
+    }
+  }).then((meal) => {
+    res.render('/show', { meal: meal });
+  });
 });
 
 app.post('/like', (req, res) => {
