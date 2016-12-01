53cab345bd9bffc78d7b84dedb50bc79d4514a98
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-12-01 16:48:36 +0100

    changed json to js

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "ela",
-    "password": null,
-    "database": "foodvote",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "test_foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": "ela",
-    "password": null,
-    "database": "foodvote",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "production_foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
