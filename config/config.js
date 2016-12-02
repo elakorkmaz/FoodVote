ce8a905a8ba6e0949a365f249be3dd6ea9332f57
marco <marcoflaco@gmail.com>
2016-12-02 11:10:13 +0100

    config

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
-{
+module.exports = {
   "development": {
-    "username": "marcocampos",
-    "password": null,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "ela",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "test_foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": "ela",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "production_foodvote",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
-}
+};
