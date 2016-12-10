ad75671f21da90103288d45a3d0db80dc43fa1ef
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-12-10 16:59:57 +0100

    added admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,103 @@
 var express = require('express'),
     db = require('../models'),
+    bcrypt = require('bcrypt'),
     router = express.Router();
 
-router.get('/', (req, res) => {
+// admin landing page ----------------------------------------------------------
+
+router.get('/menus', (req, res) => {
   db.Menu.findAll().then((menus) => {
     res.render('admin/index', { menus: menus });
   });
 });
+
+// register admin --------------------------------------------------------------
+
+router.get('/register', (req, res) => {
+  res.render('admin/new');
+});
+
+router.post('/register', (req, res) => {
+  db.User.create((req.body), {
+    where: {
+      admin: true
+    }
+  }).then(() => {
+    res.redirect('/admin/');
+  }).catch((error) => {
+    res.redirect('/admin/register');
+  });
+});
+
+// login admin -----------------------------------------------------------------
+
+router.get('/login', (req, res) => {
+  res.render('/admin/login');
+});
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+
+  db.User.findOne({
+    where: {
+      email: req.body.email,
+      admin: true
+    }
+  }).then((userInDB) => {
+    bcrypt.compare(req.body.password, userInDB.password, (error, result) => {
+      if (result) {
+        req.session.user = userInDB;
+        res.redirect('/');
+      } else {
+        res.redirect('/admin/login');
+      }
+    });
+  }).catch((error) => {
+    console.log('error occured');
+    console.log(error);
+    res.redirect('/admin/login');
+  });
+});
+
+// logout admin ----------------------------------------------------------------
+
+router.get('/logout', (req, res) => {
+  req.session.user = undefined;
+  res.redirect('/');
+});
+
+// create new menu -------------------------------------------------------------
+
+router.post('/new', (req, res) => {
+  db.Menu.create(req.body).then((Menu) => {
+    res.redirect('/' + menu.slug);
+  });
+});
+
+// edit menu -------------------------------------------------------------------
+
+router.put('/edit/:id', (req, res) => {
+  db.Menu.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    res.redirect('/admin/menus');
+  });
+});
+
+// delete menu -----------------------------------------------------------------
+
+router.delete('/menus/:id', (req, res) => {
+  db.Menu.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    res.redirect('/admin/menus');
+  });
+});
+
+// -----------------------------------------------------------------------------
 
 module.exports = router;
