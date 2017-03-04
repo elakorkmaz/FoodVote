ec55a1d525123b4aae48e6fe7831caebf793e67a
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-04 19:14:31 +0100

    heroku

--- index.js
+++ index.js
@@ -71,12 +71,6 @@ app.use(methodOverride((req, res) => {
   }})
 );
 
-app.dynamicHelpers({
-  session: function(req, res){
-    return req.session;
-  }
-});
-
 app.locals.assets = assets;
 
 // landing page general --------------------------------------------------------
