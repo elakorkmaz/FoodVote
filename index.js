46de4805c27aa64fcfdd241e9ed1a75dca0a2e34
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-04 21:00:43 +0100

    auth working

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ var client = redis.createClient();
 
 var pg = require('pg');
 
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.set('view engine', 'pug');
 
