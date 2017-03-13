0b421e0e51522c00c431342843201db3f80ea883
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-13 23:16:47 +0100

    uncommented pg

--- index.js
+++ index.js
@@ -19,24 +19,24 @@ const userRoutes = require('./routes/users'),
 
 app.set('view engine', 'pug');
 
-// var pg = require('pg');
-//
-// var conString = process.env.DATABASE_URL;
-//
-// var client = new pg.Client(conString);
-// client.connect();
+var pg = require('pg');
 
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
+var conString = process.env.DATABASE_URL;
+
+var client = new pg.Client(conString);
+client.connect();
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.use(morgan('dev'));
 
